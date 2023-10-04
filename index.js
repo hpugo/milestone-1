89e4b11cdcf594ee093f6f843b07d68d17badcac
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2023-10-03 21:06:36 -0500

    third commit

--- index.js
+++ index.js
@@ -6,3 +6,33 @@ function changeBackgroundImage() {
         console.log('Target div not found');
     }
 }
+
+var timer;
+var ele = document.getElementById('timer');
+
+(function(){
+    var sec = 0;
+    timer= setInterval(()=>{
+        ele.innerHTML='00:' +sec;
+        sec ++;
+    }, 1000)
+})
+
+
+function popBalloon() {
+    score++;
+    document.getElementById('score-value').innerText = score;
+    this.remove();
+}
+
+function updateTimer() {
+    if (timerValue > 0) {
+        timerValue--;
+        document.getElementById('timer-value').innerText = timerValue;
+        createBalloon(); // Create a new balloon every second
+        setTimeout(updateTimer, 1000);
+    } else {
+        alert('Game Over! Your score is ' + score);
+        resetGame();
+    }
+}
