613b22d80b1c8a1017a6bc7cfb1b0ab5a73281ac
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2023-10-08 12:34:22 -0500

    almost there

--- index.js
+++ index.js
@@ -1,43 +1,84 @@
+let currBalloonTile;
+let timer = 0;
 let score = 0;
-        let timer = 0;
-        let timerInterval;
+let timerInterval = 0;
+// let gameOver = false;
 
-        function setGame() {
-            let board = document.getElementById("board");
+window.onload = function() {
+    document.getElementById("startButton").addEventListener("click", startGame);
 
-            for (let i = 0; i < 9; i++) {
-                let tile = document.createElement("div");
-                tile.className = "tile";
-                tile.id = i.toString();
-                tile.addEventListener("click", function() {
-                    handleTileClick(i);
-                });
-                document.getElementById("board").appendChild(tile);
-            }
-        }
+}
 
-        function handleTileClick(tileIndex) {
-            let clickedTile = document.getElementById(tileIndex.toString());
+function setGame() {
 
-            score++;
-            document.getElementById("score").innerText = `Score: ${score}`;
+    for (let i = 0; i < 9; i++) {
+    let tile = document.createElement("div");
+         tile.id = i.toString();
+         tile.addEventListener("click", function() {
+             handleTileClick(i);
+         });
+         document.getElementById("board").appendChild(tile);
+  }
+ }
 
-            clickedTile.style.backgroundImage = "url('assets/POP!.png')";
+let board = document.getElementById("board");
 
-            if (score === 9) {
-                clearInterval(timerInterval);
+function handleTileClick(tileIndex) {
+    let clickedTile = document.getElementById(tileIndex.toString());
 
-                alert(`Congratulations! You completed the game in ${timer} seconds.`);
-            }
-        }
+    score++;
+    document.getElementById("score").innerText = `Score: ${score}`;
 
-        function startGame() {
-            setGame();
+    clickedTile.style.backgroundImage = "url('assets/singleBallonBlue.png')";
 
-            timerInterval = setInterval(function() {
-                timer++;
-                document.getElementById("timer").innerText = `Time: ${timer}`;
-            }, 1000);
-        }
+    if (score === 9) {
+        alert(`Congratulations! You completed the game in ${timer} seconds.`);
+        resetGame()
+    }
+    
+}
 
-startGame();
+function startGame() {
+    setGame();
+    clearInterval(timerInterval);
+    currBalloonTile = null;
+
+    timerInterval = setInterval(function() {
+        timer++;
+        document.getElementById("timer").innerText = `Time: ${timer}`;
+    }, 1000);
+}
+function resetGame() {
+     score = 0;
+     timer = 0;
+    clearInterval(timerInterval);
+
+    document.getElementById("score").innerText = "Score: 0";
+    document.getElementById("timer").innerText = "Time: 0";
+
+    document.getElementById("board").innerHTML = "";
+    
+}
+
+function setBalloon() {
+    if (currBalloonTile) {
+        currBalloonTile.innerHTML = "";
+    }
+
+    currBalloonTile = currBalloonTile || document.getElementById(getRandomTile());
+
+    let balloon = document.createElement("img");
+    balloon.src = "assets/singleBalloonBlue.png";
+
+    let num = getRandomTile();
+    if (currBalloonTile.id === num) {
+        return;
+    }
+    currBalloonTile = document.getElementById(num);
+    currBalloonTile.appendChild(balloon);
+}
+
+function getRandomTile() {
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
+}
