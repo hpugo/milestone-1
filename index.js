3a7f3eaa80d7c2392bc05a6a7034935c2fa26f1c
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2023-10-12 13:59:16 -0500

    final git update for M1

--- index.js
+++ index.js
@@ -1,84 +1,125 @@
-let currBalloonTile;
-let timer = 0;
+let startTime;
 let score = 0;
 let timerInterval = 0;
-// let gameOver = false;
+let tileStates = {};
+let currentLevel = 1;
 
-window.onload = function() {
-    document.getElementById("startButton").addEventListener("click", startGame);
+let timerSound = new Audio('assets/audio/3secCD.mp3');
+let gameEndSound = new Audio('assets/audio/GameEnd.mp3');
 
-}
+timerSound.load();
+gameEndSound.load();
+
+window.onload = function () {
+  document.getElementById("startButton").addEventListener("click", startCountdown);
+};
 
 function setGame() {
-
-    for (let i = 0; i < 9; i++) {
+  for (let i = 0; i < 9; i++) {
     let tile = document.createElement("div");
-         tile.id = i.toString();
-         tile.addEventListener("click", function() {
-             handleTileClick(i);
-         });
-         document.getElementById("board").appendChild(tile);
+    tile.id = i.toString();
+    tile.addEventListener("click", function () {
+      handleTileClick(i);
+    });
+    document.getElementById("board").appendChild(tile);
   }
- }
 
-let board = document.getElementById("board");
+  setBalloons();
+}
 
 function handleTileClick(tileIndex) {
-    let clickedTile = document.getElementById(tileIndex.toString());
+  let clickedTile = document.getElementById(tileIndex.toString());
 
-    score++;
-    document.getElementById("score").innerText = `Score: ${score}`;
+  if (tileStates[tileIndex]) {
+    return;
+  }
 
-    clickedTile.style.backgroundImage = "url('assets/singleBallonBlue.png')";
+  score++;
+  document.getElementById("score").innerText = `Score: ${score}`;
 
-    if (score === 9) {
-        alert(`Congratulations! You completed the game in ${timer} seconds.`);
-        resetGame()
-    }
+  let balloonClickSound = new Audio('assets/audio/POPsound.mp3');
+  balloonClickSound.volume = 0.5;
+
+  balloonClickSound.addEventListener('loadeddata', function() {
+    balloonClickSound.currentTime = 0.25;
+    balloonClickSound.play();
+  });
+
+  clickedTile.style.backgroundImage = "url('assets/POP!.png')";
+
+  tileStates[tileIndex] = true;
+
+  if (score === 9) {
+    gameEndSound.play();
     
+    let endTime = new Date().getTime();
+    let elapsedTime = (endTime - startTime) / 1000; 
+    alert(`Congratulations! You completed the game in ${elapsedTime.toFixed(3)} seconds.`);
+    resetGame();
+  }
+}
+
+function startCountdown() {
+  document.getElementById("startButton").disabled = true;
+
+  let count = 3;
+  document.getElementById("timer").innerText = count;
+
+  timerSound.play();
+
+  let countdownInterval = setInterval(function () {
+    count--;
+    document.getElementById("timer").innerText = count;
+
+    if (count === 0) {
+      clearInterval(countdownInterval);
+      document.getElementById("timer").innerText = "Go!";
+
+      setTimeout(function () {
+        document.getElementById("startButton").disabled = false;
+        startGame();
+      }, 1000);
+    }
+  }, 1000);
 }
 
 function startGame() {
-    setGame();
-    clearInterval(timerInterval);
-    currBalloonTile = null;
+  setGame();
+  clearInterval(timerInterval);
+  score = 0; 
+  document.getElementById("score").innerText = `Score: ${score}`;
+  tileStates = {};
+  currentLevel = 1;
 
-    timerInterval = setInterval(function() {
-        timer++;
-        document.getElementById("timer").innerText = `Time: ${timer}`;
-    }, 1000);
+  startTime = new Date().getTime();
+
+  timerInterval = setInterval(function () {
+    let currentTime = new Date().getTime();
+    let elapsedTime = (currentTime - startTime) / 1000; 
+    document.getElementById("timer").innerText = `Time: ${elapsedTime.toFixed(3)}`;
+  }, 50); 
 }
+
 function resetGame() {
-     score = 0;
-     timer = 0;
-    clearInterval(timerInterval);
-
-    document.getElementById("score").innerText = "Score: 0";
-    document.getElementById("timer").innerText = "Time: 0";
-
-    document.getElementById("board").innerHTML = "";
-    
+  clearInterval(timerInterval);
+  timerSound.pause();
+  timerSound.currentTime = 0;
+  document.getElementById("timer").innerText = "Time: 0";
+  document.getElementById("board").innerHTML = "";
+  setBalloons();
 }
 
-function setBalloon() {
-    if (currBalloonTile) {
-        currBalloonTile.innerHTML = "";
-    }
+let levelPatterns = {
+  1: ["assets/singleBalloonBlue.png"],
+};
 
-    currBalloonTile = currBalloonTile || document.getElementById(getRandomTile());
+function setBalloons() {
+  let patterns = levelPatterns[currentLevel] || levelPatterns[1];
 
-    let balloon = document.createElement("img");
-    balloon.src = "assets/singleBalloonBlue.png";
-
-    let num = getRandomTile();
-    if (currBalloonTile.id === num) {
-        return;
-    }
-    currBalloonTile = document.getElementById(num);
-    currBalloonTile.appendChild(balloon);
-}
-
-function getRandomTile() {
-    let num = Math.floor(Math.random() * 9);
-    return num.toString();
+  for (let i = 0; i < 9; i++) {
+    let tile = document.getElementById(i.toString());
+    tile.style.backgroundImage = "none";
+    let balloonSrc = patterns[0];
+    tile.style.backgroundImage = `url('${balloonSrc}')`;
+  }
 }
